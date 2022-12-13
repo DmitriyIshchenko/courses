4869d9aeb85bdbc9adcbde14a96a8388e2411396
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-12-13 14:29:13 +0300

    refactor: apply mvc architecture

---
+++ Forkify/src/js/model.js
@@ -0,0 +1,35 @@
+/* 
+exports are not copies but a live connection -> 
+state is also going to be updated in the controller
+*/
+export const state = {
+  recipe: {},
+};
+
+// get the data and change state (will return a promise)
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+    // format data
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
