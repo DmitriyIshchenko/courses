6987e921dcf6e3897ce6a6130c9e2da4559bf838
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2023-01-23 20:26:42 +0300

    refactor: make modal more flexible

--- react-complete-guide/users-project/src/components/Modal/ErrorModal.js
+++ react-complete-guide/users-project/src/components/Modal/ErrorModal.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Button from "../UI/Button";
+import Card from "../UI/Card";
 
 import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div
-        className={`${styles.overlay} ${!props.error && styles.hidden}`}
-        onClick={props.onClosePopup}
-      ></div>
-      <div className={`${styles.popup} ${!props.error && styles.hidden}`}>
-        <header className={styles.header}>Invalid input</header>
-        <p className={styles.message}>{props.error}</p>
-        <Button onClick={props.onClosePopup}>Okay</Button>
-      </div>
-    </div>
+    <>
+      <div className={styles.backdrop} onClick={props.onConfirm}></div>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button type="button" onClick={props.onConfirm}>
+            Okay
+          </Button>
+        </footer>
+      </Card>
+    </>
   );
 };
 
