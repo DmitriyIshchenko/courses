7fcf75b9854a196750b24269a4dd9adf1f9d5861
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-12 14:52:41 +0300

    feat: add filter method section

--- react-course-jonas/js-recap/array-methods.js
+++ react-course-jonas/js-recap/array-methods.js
@@ -66,7 +66,7 @@ const data = [
     publicationDate: "1965-01-01",
     author: "Frank Herbert",
     genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
       spanish: "",
@@ -168,4 +168,18 @@ const essentialData = books.map((book) => ({
   reviewsCount: getTotalReviewCount(book),
 }));
 essentialData;
+//!SECTION
+
+//SECTION - filter() method
+// filter and array based of condition
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
 //!SECTION
