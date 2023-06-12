552b518a1ff22e05f7e554a5e4b8cd13737724ac
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-12 14:38:12 +0300

    feat: add map method section

--- react-course-jonas/js-recap/array-methods.js
+++ react-course-jonas/js-recap/array-methods.js
@@ -143,114 +143,6 @@ function getBook(id) {
   return data.find((d) => d.id === id);
 }
 
-//SECTION - Destructuring
-
-const book = getBook(1);
-console.log(book);
-
-// const title = book.title;
-// const author = book.author;
-
-const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
-  book;
-console.log(author, title, genres);
-
-// const primaryGenre = genres[0];
-// const secondaryGenre = genres[1];
-
-const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
-console.log(primaryGenre, secondaryGenre, otherGenres);
-// !SECTION
-
-//SECTION - Rest/spread operator
-//* Rest - left side, Spread - right side
-
-// Array example
-const newGenres = ["epic fantasy", ...genres];
-console.log(newGenres);
-
-// Object example
-const updatedBook = {
-  ...book,
-  // Adding a new property
-  moviePublicationDate: "2001-12-19",
-  // Overwriting an existing property (have to be after spread object)
-  pages: 1210,
-};
-console.log(updatedBook);
-//!SECTION
-
-//SECTION - Arrow functions
-// Don't have this keyword and arguments array
-
-// function getYear(str) {
-//   return str.split("-")[0];
-// }
-
-const getYear = (str) => str.split("-")[0];
-console.log(getYear(publicationDate));
-
-//!SECTION
-
-//SECTION Template literals
-
-const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
-  publicationDate
-)}. The book has ${hasMovieAdaptation ? "" : "not "}been adapted as a movie`;
-console.log(summary);
-
-//!SECTION
-
-//SECTION - Ternaries
-
-const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
-console.log(pagesRange);
-console.log(`The book has ${pagesRange} pages`);
-
-//!SECTION
-
-//SECTION - Short-circuiting and logical operators
-
-//* AND
-// short-circuits whenever the first operator is false and returns it
-console.log(true && "Some string");
-console.log(false && "Some string");
-console.log(hasMovieAdaptation && "This book has a movie");
-
-// falsy values: 0, '', null, undefined
-
-// use case: instead of IF operator
-console.log("jonas" && "some string");
-console.log(0 && "some string");
-
-//* OR - the opposite of AND
-// short-circuits whenever the first operator is true and returns it
-
-console.log(true || "some string");
-console.log(false || "some string");
-
-// use case: set default values
-console.log(book.translations.spanish);
-const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
-
-console.log(spanishTranslation);
-
-// pitfall
-console.log(book.reviews.librarything.reviewsCount);
-const countWrong = book.reviews.librarything.reviewsCount || "no data";
-
-// we want it to be 0 instead of 'no data'
-console.log(countWrong);
-
-//* Nullish coalescing operator - solution
-// return the second value when the first value is null or undefined, but not if it's 0 or ""
-const count = book.reviews.librarything.reviewsCount ?? "no-data";
-console.log(count);
-
-//!SECTION
-
-//SECTION - Optional chaining
-
 function getTotalReviewCount(book) {
   const goodreads = book.reviews?.goodreads.reviewsCount ?? 0;
   const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
@@ -258,6 +150,22 @@ function getTotalReviewCount(book) {
   return goodreads + librarything;
 }
 
-console.log(getTotalReviewCount(book));
+const books = getBooks();
+books;
 
+//SECTION - map() method
+// creates new array based on original array with some operation applied
+
+const x = [1, 2, 3].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+console.log(titles);
+
+const essentialData = books.map((book) => ({
+  author: book.author,
+  title: book.title,
+  reviewsCount: getTotalReviewCount(book),
+}));
+essentialData;
 //!SECTION
