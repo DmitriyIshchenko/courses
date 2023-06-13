1b4aaa07436879091c8b717db0b2de6ab034f3d6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-13 10:10:38 +0300

    init: start profile-card challenge

---
+++ react-course-jonas/profile-card-challenge/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <div></div>;
+}
+
+function Intro() {
+  return <div></div>;
+}
+
+function SkillList() {
+  return <div></div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
