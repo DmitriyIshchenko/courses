3b633bf34446437580d633d4a4bebd552abacb0c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-13 17:16:26 +0300

    init: start date counter challenge

---
+++ react-course-jonas/date-counter-challenge/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  return <div className="counter">init</div>;
+}
+export default App;
