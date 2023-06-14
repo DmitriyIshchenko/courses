f561f577ccfca21873cb3f12595a54634f9342f6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-14 09:19:10 +0300

    feat: add step and count states

--- react-course-jonas/date-counter-challenge/src/App.js
+++ react-course-jonas/date-counter-challenge/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="App">
@@ -7,6 +9,36 @@ function App() {
 }
 
 function Counter() {
-  return <div className="counter">init</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const handleDecreaseStep = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  const handleIncreaseStep = () => {
+    setStep((s) => s + 1);
+  };
+  const handleDecreaseCount = () => {
+    if (count >= step) setCount((c) => c - step);
+  };
+  const handleIncreaseCount = () => {
+    setCount((c) => c + step);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <button onClick={handleDecreaseStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleIncreaseStep}>+</button>
+      </div>
+
+      <div>
+        <button onClick={handleDecreaseCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleIncreaseCount}>+</button>
+      </div>
+    </div>
+  );
 }
 export default App;
