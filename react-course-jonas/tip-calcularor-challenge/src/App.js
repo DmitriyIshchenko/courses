72153686c42072c5c6f46dee80c98770c12d3ee5
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-15 12:18:02 +0300

    feat: create static components

--- react-course-jonas/tip-calcularor-challenge/src/App.js
+++ react-course-jonas/tip-calcularor-challenge/src/App.js
@@ -1,5 +1,39 @@
-function App() {
-  return <div className="App">TODO</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <BillInput />
+      <TipInput></TipInput>
+      <TipInput></TipInput>
+      <Output />
+      <ResetButton />
+    </div>
+  );
 }
 
-export default App;
+function BillInput() {
+  return (
+    <div>
+      <span>How much was the bill?</span>
+      <input type="text" />
+    </div>
+  );
+}
+function TipInput() {
+  return (
+    <div>
+      <span>How did you like the service?</span>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">Ok (5%)</option>
+        <option value="10">Good (10%)</option>
+        <option value="20">Amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+function Output() {
+  return <h2>You pay X ($Y + $Z tip)</h2>;
+}
+function ResetButton() {
+  return <button>Reset</button>;
+}
