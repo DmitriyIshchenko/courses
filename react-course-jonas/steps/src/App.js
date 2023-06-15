7ff00aa051217a6ce6893b990f54e08028573711
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-15 10:06:34 +0300

    refactor: create reusable message component

--- react-course-jonas/steps/src/App.js
+++ react-course-jonas/steps/src/App.js
@@ -33,10 +33,23 @@ export default function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
+          {/* 
           <p className="message">
             Step {step}: {messages[step - 1]}
-          </p>
+          </p> */}
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -61,5 +74,14 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
