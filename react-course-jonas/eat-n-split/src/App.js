ad105a8468fe0185f1485395884bafc24c51d6ef
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-15 18:39:19 +0300

    feat: toggle add friend form display

--- react-course-jonas/eat-n-split/src/App.js
+++ react-course-jonas/eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,13 +22,22 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
+
       <FormSplitBill />
     </div>
   );
