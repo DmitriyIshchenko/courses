40977da38ae0853fb6c0bff16ca9d8c235b3a4e6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-20 11:22:06 +0300

    feat: control inputs

--- react-course-jonas/currency-converter-challenge/src/App.js
+++ react-course-jonas/currency-converter-challenge/src/App.js
@@ -1,16 +1,31 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useState } from "react";
 
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [selectedFrom, setSelectedFrom] = useState("USD");
+  const [selectedTo, setSelectedTo] = useState("EUR");
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(+e.target.value)}
+      />
+      <select
+        value={selectedFrom}
+        onChange={(e) => setSelectedFrom(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={selectedTo}
+        onChange={(e) => setSelectedTo(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
