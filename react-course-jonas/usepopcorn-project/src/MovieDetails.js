c2f01141e3ce918bd15955101d2ef19dbacdb65e
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-21 11:34:45 +0300

    refactor: extract components into separate files

---
+++ react-course-jonas/usepopcorn-project/src/MovieDetails.js
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import StarRating from "./StarRating";
+import { KEY } from "./App";
+import ErrorMessage from "./ErrorMessage";
+import Loader from "./Loader";
+
+export default function MovieDetails({
+  selectedId,
+  onCloseMovie,
+  onAddWatched,
+  watched,
+}) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const [userRating, setUserRating] = useState(0);
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId
+  )?.userRating;
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbID: selectedId,
+      poster,
+      title,
+      runtime: +runtime.split(" ")[0] || 90,
+      imdbRating: +imdbRating,
+      userRating,
+    };
+
+    onAddWatched(newWatchedMovie);
+    onCloseMovie();
+  }
+
+  /* Hooks experiments (how not to call hooks)
+  
+  Conditional calling:
+  if(imdbRating > 8) [isTop, setIsTop] = useState(false);
+ 
+  Early return:
+  if(imdbRating > 8) return <p>Great!</p>
+  */
+  // imdb rating is still undefined on mount
+  // (will state false forever without manual update)
+  // const [isTop, setIsTop] = useState(imdbRating > 5);
+  // console.log(isTop);
+  // works but we should use the derived state instead
+  // useEffect(() => {
+  //   setIsTop(imdbRating > 5);
+  // }, [imdbRating]);
+  // const isTop = imdbRating > 5;
+  /* close tab on Esc press */
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === "Escape") onCloseMovie();
+    }
+    document.addEventListener("keydown", callback);
+
+    // must cleanup the event listener to prevent handlers accumulating
+    return () => document.removeEventListener("keydown", callback);
+  }, [onCloseMovie]);
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return () => {
+      document.title = "usePopcorn";
+    };
+  }, [title]);
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movie");
+
+          const data = await res.json();
+          if (data.Response === "False") {
+            throw new Error(data.Error);
+          }
+
+          setMovie(data);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      getMovieDetails();
+    },
+    [selectedId]
+  );
+
+  return (
+    <div className="details">
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+      {!isLoading && !error && (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`${title} poster`} />
+
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                {imdbRating} IMDB rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              {isWatched ? (
+                <p>
+                  You rated this movie {watchedUserRating} <span>⭐️</span>
+                </p>
+              ) : (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRating={setUserRating}
+                  />
+
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      + Add to list
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
