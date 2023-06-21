d145acd82f3b1c2023c8eef5eec758adad01a317
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-21 13:19:25 +0300

    feat: add focus on search field

--- react-course-jonas/usepopcorn-project/src/components/NavBar/Search.js
+++ react-course-jonas/usepopcorn-project/src/components/NavBar/Search.js
@@ -1,4 +1,34 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  // not react way to do things
+  // useEffect(() => {
+  // const el = document.querySelector(".search");
+  // console.log(el);
+  // el.focus();
+  // }, []);
+
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      // focused element
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+
+    inputEl.current.focus();
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +36,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
