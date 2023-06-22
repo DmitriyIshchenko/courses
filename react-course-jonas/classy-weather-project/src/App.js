e7817465b2ee1e9d9cd9f674659a7d86725d5399
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-22 19:10:18 +0300

    feat: control input

--- react-course-jonas/classy-weather-project/src/App.js
+++ react-course-jonas/classy-weather-project/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "lisbon",
+    };
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
 
-        <input type="text" placeholder="Search for location..." />
+        <input
+          type="text"
+          placeholder="Search for location..."
+          value={this.state.location}
+          onChange={(e) => this.setState({ location: e.target.value })}
+        />
+
+        <button>Get weather</button>
       </div>
     );
   }
