118613398f09371b0e61040a0d4c10bca459cf93
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-06-22 18:37:08 +0300

    feat: set state

--- react-course-jonas/classy-weather-project/src/App.js
+++ react-course-jonas/classy-weather-project/src/App.js
@@ -7,14 +7,40 @@ class Counter extends React.Component {
     this.state = {
       count: 5,
     };
+
+    // give methods access to component instance
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  /*
+  when react call the event handler in JSX,
+  it first creates a copy of this function -> the function call is normal function call, that is not bound to any object -> this keyword is lost
+  */
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div className="">
-        <button>-</button>
+        <button onClick={this.handleDecrement}>-</button>
         <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleIncrement}>+</button>
+        <p>
+          {this.state.count} days from now is {date.toDateString()}
+        </p>
       </div>
     );
   }
