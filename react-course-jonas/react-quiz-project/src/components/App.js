6f901c0eb10e0815de96d3edaebef366f34166d5
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-15 12:37:57 +0300

    feat: handle restarting the quiz

--- react-course-jonas/react-quiz-project/src/components/App.js
+++ react-course-jonas/react-quiz-project/src/components/App.js
@@ -50,6 +50,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        status: "ready",
+        highscore: state.highscore,
+        questions: state.questions,
+      };
     default:
       throw new Error("Unknown action");
   }
@@ -116,6 +123,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
