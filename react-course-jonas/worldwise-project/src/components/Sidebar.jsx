1ac033b59f395f941f0b49a7f2c59a0b30a260cc
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-16 09:34:14 +0300

    feat: add nested routes for app navigation

--- react-course-jonas/worldwise-project/src/components/Sidebar.jsx
+++ react-course-jonas/worldwise-project/src/components/Sidebar.jsx
@@ -3,14 +3,15 @@ import AppNav from "./AppNav";
 import Footer from "./Footer";
 
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <Logo />
       <AppNav />
+      <Outlet />
 
-      <p>list of cities</p>
       <Footer />
     </aside>
   );
