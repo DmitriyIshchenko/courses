47e78f5edd4e8b8c186e73a6bd41705e3451d919
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-17 10:40:34 +0300

    feat: synchronize search parameters with the map position state

--- react-course-jonas/worldwise-project/src/components/Map.jsx
+++ react-course-jonas/worldwise-project/src/components/Map.jsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
 
 function Map() {
   const { cities } = useCities();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [mapPosition] = useState([40, 0]);
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -37,9 +41,17 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+
+  map.setView(position);
+  return null;
 }
 
 export default Map;
