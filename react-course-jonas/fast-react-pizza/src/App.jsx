b9b4a669232596f33f9aa51c61245d6529a39454
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-10 13:08:38 +0300

    feat: handle errors

--- react-course-jonas/fast-react-pizza/src/App.jsx
+++ react-course-jonas/fast-react-pizza/src/App.jsx
@@ -6,17 +6,20 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 // AppLayout is called a layout route since it doesn't have the specified path
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
