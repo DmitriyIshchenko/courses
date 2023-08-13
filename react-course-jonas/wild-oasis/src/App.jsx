c86d422a7065a5ea2f97d8f68a8f6666f386f194
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-13 15:19:01 +0300

    feat: create styled app component

--- react-course-jonas/wild-oasis/src/App.jsx
+++ react-course-jonas/wild-oasis/src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobasStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+// tagged template literals
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The wild oasis</H1>
+        <Button onClick={() => alert("1")}>Click me</Button>
+        <Button onClick={() => alert("1")}>Click me</Button>
+
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
