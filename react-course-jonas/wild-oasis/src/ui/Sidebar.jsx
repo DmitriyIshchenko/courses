604b0e3f4da0a8947f7e673dc982b688cf40637d
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-13 17:30:39 +0300

    feat: build the navigation

--- react-course-jonas/wild-oasis/src/ui/Sidebar.jsx
+++ react-course-jonas/wild-oasis/src/ui/Sidebar.jsx
@@ -1,7 +1,14 @@
 import { styled } from "styled-components";
 
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
 const StyledSidebar = styled.aside`
   grid-row: 1/-1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 
   border-right: 1px solid var(--color-grey-100);
   padding: 3.2rem 2.4rem;
@@ -9,7 +16,12 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
-  return <StyledSidebar>sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
