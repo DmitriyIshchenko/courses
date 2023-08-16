f28b4f1efef80309c4974202594b71c57a6fef27
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-16 18:35:00 +0300

    feat: use the highe-order components pattern

--- react-course-jonas/render-props-pattern/src/App.jsx
+++ react-course-jonas/render-props-pattern/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
+
+import withToggles from "./HOC";
+
 import "./styles.css";
 
 const products = Array.from({ length: 20 }, () => {
@@ -74,6 +77,19 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
+const ProductListWithToggles = withToggles(ProductList);
+
 export default function App() {
   return (
     <div>
@@ -99,6 +115,10 @@ export default function App() {
             />
           )}
         />
+      </div>
+      <div className="col-2">
+        <ProductList items={products} title="products" />
+        <ProductListWithToggles items={products} title="Products HOC" />
       </div>
     </div>
   );
