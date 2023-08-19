44d1abde5c826377eceb06df05535257b647447f
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-19 17:46:58 +0300

    feat: create a modal component

--- react-course-jonas/wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ react-course-jonas/wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import Textarea from "../../ui/Textarea";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
   const isWorking = isCreating || isEditing;
@@ -40,6 +40,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
             onSuccess: () => reset(),
           }
         );
+
+    onCloseModal?.();
   }
 
   function onError(error) {
@@ -47,7 +49,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -133,7 +138,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
