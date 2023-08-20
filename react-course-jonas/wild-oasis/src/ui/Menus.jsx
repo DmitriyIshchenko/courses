40575824b79bb214291f339491e5eb0490019bfc
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-20 14:38:04 +0300

    fix: fix menu toggle bug

--- react-course-jonas/wild-oasis/src/ui/Menus.jsx
+++ react-course-jonas/wild-oasis/src/ui/Menus.jsx
@@ -99,6 +99,7 @@ function Toggle({ id }) {
   const { openId, open, close, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     // ignore svg icon
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
@@ -117,7 +118,7 @@ function Toggle({ id }) {
 }
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
