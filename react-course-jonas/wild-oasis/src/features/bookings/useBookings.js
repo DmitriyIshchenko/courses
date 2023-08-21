67484dd25f373780ab5eb870303213486af8b491
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-21 17:36:40 +0300

    feat: pre-fetch bookings

--- react-course-jonas/wild-oasis/src/features/bookings/useBookings.js
+++ react-course-jonas/wild-oasis/src/features/bookings/useBookings.js
@@ -1,9 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
-  // get params for API-side filtering
+  const queryClient = useQueryClient();
+
+  // get params for API-side filtering/sorting
   const [searchParams] = useSearchParams();
 
   // FILTER (server side)
@@ -19,9 +22,10 @@ export function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  // PAGINATION
+  // PAGINATION (server side)
   const page = !searchParams.get("page") ? 1 : +searchParams.get("page");
 
+  // QUERY
   const {
     data: { data: bookings, count } = {},
     isLoading,
@@ -30,6 +34,24 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, page }),
     queryKey: ["bookings", filter, sortBy, page], // add "dependencies" to re-fetch
   });
+
+  // PRE-FETCHING (edge cases: first and last pages)
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  // pre-fetch the next page
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+      queryKey: ["bookings", filter, sortBy, page + 1], // add "dependencies" to re-fetch
+    });
+
+  // pre-fetch the prev page
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+      queryKey: ["bookings", filter, sortBy, page - 1], // add "dependencies" to re-fetch
+    });
 
   return { isLoading, bookings, error, count };
 }
