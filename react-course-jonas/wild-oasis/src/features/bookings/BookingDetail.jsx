f71ba62f7a0deb1456a574d424e7b6507be91b1a
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-08-22 16:55:55 +0300

    feat: delete bookings

--- react-course-jonas/wild-oasis/src/features/bookings/BookingDetail.jsx
+++ react-course-jonas/wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -9,11 +9,14 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 import Empty from "../../ui/Empty";
 import Spinner from "../../ui/Spinner";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -25,7 +28,8 @@ function BookingDetail() {
   const navigate = useNavigate();
   const moveBack = useMoveBack();
   const { checkout, isCheckingOut } = useCheckout();
-  const { booking = {}, isLoading, error } = useBooking();
+  const { booking, isLoading, error } = useBooking();
+  const { deleteBooking, isDeletingBooking } = useDeleteBooking();
 
   if (isLoading) return <Spinner />;
   if (error) return <Empty resourceName="booking" />;
@@ -50,23 +54,44 @@ function BookingDetail() {
 
       <BookingDataBox booking={booking} />
 
-      <ButtonGroup>
-        <Button $variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-
-        {status === "unconfirmed" && (
-          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
-            Check in
+      <Modal>
+        <ButtonGroup>
+          <Button $variation="secondary" onClick={moveBack}>
+            Back
           </Button>
-        )}
 
-        {status === "checked-in" && (
-          <Button onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
-            Check out
-          </Button>
-        )}
-      </ButtonGroup>
+          {status === "unconfirmed" && (
+            <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+              Check in
+            </Button>
+          )}
+
+          {status === "checked-in" && (
+            <Button
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckingOut}
+            >
+              Check out
+            </Button>
+          )}
+
+          <Modal.Open opens="delete">
+            <Button $variation="danger">Delete</Button>
+          </Modal.Open>
+        </ButtonGroup>
+
+        <Modal.Window name="delete">
+          <ConfirmDelete
+            resourceName="booking"
+            onConfirm={() => {
+              deleteBooking(bookingId, {
+                onSettled: moveBack,
+              });
+            }}
+            disabled={isDeletingBooking}
+          />
+        </Modal.Window>
+      </Modal>
     </>
   );
 }
