3d4af49c8f4a7f129e4f924718f89fa20b70183b
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-09-30 15:16:00 +0300

    feat: compute recent bookings and stays

--- react-course-jonas/wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ react-course-jonas/wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
+import Spinner from "../../ui/Spinner";
+
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoadingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoadingStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today activity</div>
+      <div>Chart stay durations </div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
