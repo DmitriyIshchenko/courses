f382f3c00d14013941f8678b3f90bf041b2c0b1f
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-10-06 13:35:03 +0300

    feat: avoid implicit any

--- typescript-course/Types/variables.ts
+++ typescript-course/Types/variables.ts
@@ -23,3 +23,31 @@ tvShow = "Breaking bad";
 let isFunny = false;
 isFunny = true;
 // isFunny = 'fsdfsdf'; // Error
+
+// the any type
+
+// everything is valid syntax
+let thing: any = "hello";
+thing = 1;
+thing = false;
+thing();
+thing.toUpperCase();
+
+// let thing = 'hello';
+// thing(); // Error
+// thing.sdfskdf(); // Error
+
+const movies = ["Arrival", "Thi Thing", "The Matrix"];
+// let foundMovie; // implicit any
+let foundMovie: string; // delayed initialization
+
+for (let movie of movies) {
+  if (movie === "The Matrix") {
+    foundMovie = "The Matrix";
+  }
+}
+// TS is not complaining about this because of implicit any
+// but it is in case of delayed initialization
+// foundMovie();
+// foundMovie = 1;
+// foundMovie.asdaff();
