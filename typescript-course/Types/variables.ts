6995d34cfeb8186ddfa052f1274e5c5eaa0b7783
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-10-06 12:45:31 +0300

    feat: annotate number and boolean types

--- typescript-course/Types/variables.ts
+++ typescript-course/Types/variables.ts
@@ -1,5 +1,16 @@
+// Strings
 let movieTitle: string = "Amadeus";
 movieTitle = "Arrival";
-movieTitle = 9; // Error
-movieTitle.upper(); // Error
+// movieTitle = 9; // Error
+// movieTitle.upper(); // Error
 movieTitle.toUpperCase(); // OK
+
+// Numbers
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "zero"; // Error
+
+// Booleans
+let gameOver: boolean = false;
+gameOver = true;
+// gameOver = 'true'; // Error
