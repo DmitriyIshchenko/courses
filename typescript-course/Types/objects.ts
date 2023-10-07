e93bf9446692c71cefe18113a6a39581daf4a22d
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-10-07 23:19:22 +0300

    feat: add type alias example

--- typescript-course/Types/objects.ts
+++ typescript-course/Types/objects.ts
@@ -29,3 +29,30 @@ function randomCoordinate(): { x: number; y: number } {
 // NO ERROR - passing the variable with extra data is fine
 const character = { first: "Jesse", last: "Pinkman", age: 22 };
 printName(character); // NO ERROR
+
+// Type aliases
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+// function doublePoint(point: { x: number; y: number }): {
+//   x: number;
+//   y: number;
+// } {
+//   return {
+//     x: point.x * 2,
+//     y: point.y * 2,
+//   };
+// }
+
+// reuse type
+let coord: Point = { x: 1, y: 2 };
+
+function doublePoint(point: Point): Point {
+  return {
+    x: point.x * 2,
+    y: point.y * 2,
+  };
+}
