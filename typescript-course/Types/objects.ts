47b05d84f1afa04a1951861be2ae6284b738a6aa
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-17 20:33:56 +0300

    feat: add intersection type example

--- typescript-course/Types/objects.ts
+++ typescript-course/Types/objects.ts
@@ -111,3 +111,40 @@ const user: User = {
 
 console.log(user.id);
 // user.id = 2; // ERROR
+
+// INTERSECTION TYPES
+
+type Circle = {
+  radius: number;
+};
+
+type Colorful = {
+  color: string;
+};
+
+type ColorfulCircle = Circle & Colorful;
+
+const happyFace: ColorfulCircle = {
+  radius: 4,
+  color: "yellow",
+};
+
+type Cat = {
+  numLives: number;
+};
+
+type Dog = {
+  breed: string;
+};
+
+// we can add "inline" types
+type CatDog = Cat &
+  Dog & {
+    age: number;
+  };
+
+const christy: CatDog = {
+  numLives: 7,
+  breed: "husky",
+  age: 9,
+};
