dd1eb42bc17256035637e5c15354a8835394aa11
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-27 11:35:14 +0300

    feat: add array example

--- typescript-course/src/index.ts
+++ typescript-course/src/index.ts
@@ -2,5 +2,17 @@
 const person = {
   name: "john",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
-console.log(person.name);
+
+let favoriteActivities: string[];
+// favoriteActivities = "sports"; // Error
+// favoriteActivities = ["sports", 1]; //Error
+favoriteActivities = ["sports"];
+favoriteActivities = ["sports", "a"];
+console.log(person.name, favoriteActivities);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()) // Error
+}
