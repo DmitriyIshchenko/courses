7bb895a6dbfb5f3433ae2205d8ef563372672eb9
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-28 11:42:18 +0300

    feat: add tuples example

--- typescript-course/src/index.ts
+++ typescript-course/src/index.ts
@@ -1,10 +1,22 @@
 // const person: { name: string; age: number } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "john",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"], // infers array, need tuple
 };
 
+// TUPLES
+// person.role.push("admin"); // exception that allowed in tuples
+// person.role[1] = 1; // Error
+// person.role = [1, "admin", 0]; // Error
+
+// ARRAYS
 let favoriteActivities: string[];
 // favoriteActivities = "sports"; // Error
 // favoriteActivities = ["sports", 1]; //Error
