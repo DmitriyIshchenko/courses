ea687e5e50a7b55cf14805b35219ec3382a345c0
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-29 12:13:38 +0300

    feat: add literal types example

--- typescript-course/src/index.ts
+++ typescript-course/src/index.ts
@@ -1,16 +1,33 @@
-function combine(input1: number | string, input2: number | string) {
+/* eslint-disable prettier/prettier */
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   //runtime check
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString;
+  // }
 }
 
-const combineAges = combine(12, 37);
+const combineAges = combine(12, 37, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("John", "Jane");
+const combineStringAges = combine("12", "37", "as-number");
+console.log(combineStringAges);
+
+const combineNames = combine("John", "Jane", "as-text");
 console.log(combineNames);
