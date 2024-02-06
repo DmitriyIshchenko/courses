b7d229f84cd6b1e09a2eecbb105d0eb4a1db3a25
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-06 19:03:59 +0300

    feat: add inheritance example

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -18,10 +18,38 @@ class Department {
   }
 }
 
-const accounting = new Department("a1", "Accounting");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const IT = new ITDepartment("i1", ["Max"]);
+console.log(IT);
+IT.describe();
+
+const accounting = new AccountingDepartment("a1", ["report1"]);
 accounting.addEmployee("john");
-accounting.addEmployee("john");
+accounting.addEmployee("pete");
 // accounting.employees.push("anna"); // BAD idea, make private
+accounting.addReport("report2");
+accounting.printReports();
 
 accounting.describe();
 accounting.printEmployeesInfo();
