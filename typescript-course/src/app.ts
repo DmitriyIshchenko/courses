afd4bc390115b95ed3fbcd3fed0c8c71d018be84
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-14 22:53:47 +0300

    feat: add type guards example

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -22,3 +22,67 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // union types - types in common (number in this case)
+
+// TYPE GUARDS
+
+function add(a: Combinable, b: Combinable) {
+  // type guard
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(`Name: ${emp.name}`);
+
+  // check if property exists
+  if ("privileges" in emp) {
+    console.log(`Privileges: ${emp.privileges}`);
+  }
+
+  if ("startDate" in emp) {
+    console.log(`StartDate: ${emp.startDate}`);
+  }
+}
+
+printEmployeeInformation({ name: "Jack", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`loading cargo: ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // type guard
+  // if ("loadCargo" in vehicle) {
+  //   vehicle.loadCargo(12);
+  // }
+
+  // type guard
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(12);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
