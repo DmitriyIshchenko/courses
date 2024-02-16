ab0996094154797b506a074df439c037ca713a2c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-16 11:05:56 +0300

    feat: use discrinimating unions pattern

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -86,3 +86,33 @@ function useVehicle(vehicle: Vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+
+// DISCRIMINATING UNIONS
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log(`Moving at ${speed} MPH`);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 12 });
