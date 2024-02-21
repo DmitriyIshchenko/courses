fc85b29048207c8d16e0348b855683f49b90618d
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-21 16:43:04 +0300

    feat: add another generec function with type constraint

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -20,3 +20,22 @@ const mergedObj = merge({ name: "John", hobbies: ["Sports"] }, { age: 30 });
 // );
 
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+
+  if (element.length === 1) {
+    descriptionText = `Got 1 element`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe([]));
+console.log(countAndDescribe([1, 2, 3, 4]));
+console.log(countAndDescribe("hello"));
