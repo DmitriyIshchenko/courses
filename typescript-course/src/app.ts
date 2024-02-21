2ac51799c3a3965a7f6a4682f4d09ab5eb2733f6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-21 16:35:15 +0300

    feat: add constraints

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -1,7 +1,7 @@
 // GENERIC FUNCTION
 
 // T for type, U - next in alphabet
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign({}, objA, objB);
 }
 
@@ -10,13 +10,13 @@ function merge<T, U>(objA: T, objB: U) {
 //   age: number;
 // }; // BAD
 
-const mergedObj = merge({ name: "John", hobbies: ["Sports"] }, { age: 33 });
-const mergedObj2 = merge({ name: "Steve" }, { age: 64 });
+const mergedObj = merge({ name: "John", hobbies: ["Sports"] }, { age: 30 });
+// const mergedObj2 = merge({ name: "Steve" }, { age: 64 });
 
 // tell which type it should fill in (redundant)
-const mergedObj3 = merge<{ name: string; hobbies: string[] }, { age: number }>(
-  { name: "Sam", hobbies: ["Sports"] },
-  { age: 55 }
-);
+// const mergedObj3 = merge<{ name: string; hobbies: string[] }, { age: number }>(
+//   { name: "Sam", hobbies: ["Sports"] },
+//   { age: 55 }
+// );
 
-console.log(mergedObj, mergedObj2, mergedObj3);
+console.log(mergedObj);
