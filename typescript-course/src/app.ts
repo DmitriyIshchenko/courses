4bd3c0c82a2601314f1abaef1c4a026252bfd99c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-03-01 16:18:05 +0300

    feat: create an autobind decorator

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -49,6 +49,7 @@ console.log(pers);
 // ----
 
 // target is prototype of the object (for static property - constructor function)
+// typescript will ignore return
 function Log(target: any, propertyName: string | Symbol) {
   console.log("property decorator");
   console.log(target, propertyName);
@@ -72,8 +73,11 @@ function Log3(
   console.log(target);
   console.log(name);
   console.log(descriptor);
+
+  // can return new descriptor that will replace the old one
 }
 
+// typescript will ignore return
 function Log4(
   target: any,
   name: string | Symbol, //name of the method
@@ -109,3 +113,36 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    // run extra logic on access to this property
+    get() {
+      // this refers to whatever is responsible for triggering the getter -> always refers to the object
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjustedDescriptor;
+}
+
+class Printer {
+  message = "this works!";
+
+  @autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+// button.addEventListener("click", p.showMessage); // undefined
+// button.addEventListener("click", p.showMessage.bind(p)); // default js way
+button.addEventListener("click", p.showMessage);
