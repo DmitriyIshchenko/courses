60eebe1d4076ddf9cc0643ba3708136dbcec522b
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-03-04 17:11:45 +0300

    feat: confugure form

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -2,23 +2,55 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
+    // get template
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
+    // get app div
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
+    // get template content (form itself)
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    // populate fields
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+
+    // render form
     this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  private submitHandler(e: Event) {
+    e.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 }
 
