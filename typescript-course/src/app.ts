a3984ad17580dbc3d051bd7e79f0c875fb979626
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-03-05 10:45:32 +0300

    feat: render project lists

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -65,6 +65,47 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor;
 }
 
+//////////////////////////////////
+// PROJECT LIST
+//////////////////////////////////
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    // get template
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    // get app div
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // get template content (form itself)
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 ////////////////////////////
 // FORM
 ////////////////////////////
@@ -178,3 +219,5 @@ class ProjectInput {
 }
 
 const projInput = new ProjectInput();
+const activeProjList = new ProjectList("active");
+const finishedProjList = new ProjectList("finished");
