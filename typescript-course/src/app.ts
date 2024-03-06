c2aecea85baf626737d8208badaf9d30f507f075
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-03-06 17:57:12 +0300

    refactor: create project item class

--- typescript-course/src/app.ts
+++ typescript-course/src/app.ts
@@ -186,6 +186,25 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 //////////////////////////////////
 // PROJECT LIST
 //////////////////////////////////
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  constructor(hostId: string, private project: Project) {
+    super("single-project", hostId, false, project.id);
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(): void {}
+
+  renderContent(): void {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent =
+      this.project.people.toString();
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
@@ -233,9 +252,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
     // render updated data
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = projectItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(listEl.id, projectItem);
     }
   }
 }
