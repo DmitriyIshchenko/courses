558ec43842accd11bd60b1144183eca5c24f9293
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-16 14:21:58 +0300

    chore(044): practice autocomplete

--- total-typescript-wokshop/src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
+++ total-typescript-wokshop/src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
@@ -8,12 +8,15 @@ const acceptsObj = (obj: MyObj) => {};
 
 acceptsObj({
   // Autocomplete in here!
+  foo: "sfsdf",
+  bar: 12,
+  baz: true,
 });
 
 document.addEventListener(
   // Autocomplete this string!
-  "",
+  "DOMContentLoaded",
   (event) => {
     console.log(event);
-  },
+  }
 );
