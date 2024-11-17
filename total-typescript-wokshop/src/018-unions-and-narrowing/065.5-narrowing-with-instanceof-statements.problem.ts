f8242dec81ff7b55358485eda5a0a8ce3bb6842b
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-17 20:10:22 +0300

    fix(065.5): use intanceof to narrow error type

--- total-typescript-wokshop/src/018-unions-and-narrowing/065.5-narrowing-with-instanceof-statements.problem.ts
+++ total-typescript-wokshop/src/018-unions-and-narrowing/065.5-narrowing-with-instanceof-statements.problem.ts
@@ -1,6 +1,7 @@
 const somethingDangerous = () => {
   if (Math.random() > 0.5) {
     throw new Error("Something went wrong");
+    // throw "Something went wrong";
   }
 
   return "all good";
@@ -11,7 +12,9 @@ try {
 } catch (error) {
   // How do we change this code to make it
   // not show a red squiggly?
-  if (true) {
+  if (error instanceof Error) {
     console.error(error.message);
+  } else {
+    throw error; // cover the case when just the string was thrown (line 4)
   }
 }
