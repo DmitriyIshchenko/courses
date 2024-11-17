b2ce343992277598d8817baa634392eb7b31cebd
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-17 18:29:26 +0300

    refactor(062): throw an error if there is no app element

--- total-typescript-wokshop/src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ total-typescript-wokshop/src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -3,5 +3,6 @@ import { Equal, Expect } from "@total-typescript/helpers";
 const appElement = document.getElementById("app");
 
 // How do I ensure that appElement is defined?
+if (!appElement) throw new Error("No app element!");
 
 type Test = Expect<Equal<typeof appElement, HTMLElement>>;
